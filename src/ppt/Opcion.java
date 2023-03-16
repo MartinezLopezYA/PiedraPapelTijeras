9d0878abfbf0cf045c3ae85a10392ecc814e9702
MartinezLopezYA <martinezlopezyersonandres.ing@gmail.com>
2023-03-16 09:14:42 -0500

    Modificaciones y archivos faltantes

--- src/ppt/Opcion.java
+++ src/ppt/Opcion.java
@@ -39,33 +39,39 @@ public class Opcion {
             String b = "Papel";
             System.out.println("J1 saco: " + a + "\nJ2 saco: " + b + " \nPunto para j2");
             this.p2+=1;
+            System.out.println();
         } else if (x == 2 && y == 1) {
             String a = "Piedra";
             String b = "Papel";
             System.out.println("J1 saco: " + b + "\nJ2 saco: " + a + " \nPunto para j1");
             this.p1+=1;
+            System.out.println();
         } else if (x == 1 && y == 3) {
             String a = "Piedra";
             String c = "Tijera";
             System.out.println("J1 saco: " + a + "\nJ2 saco: " + c + " \nPunto para J1");
             this.p1+=1;
+            System.out.println();
         } else if (x == 3 && y == 1) {
             String a = "Piedra";
             String c = "Tijera";
             System.out.println("J1 saco: " + c + "\nJ2 saco: " + a + "\nPunto para J2");
             this.p2+=1;
+            System.out.println();
         } else if (x == 2 && y == 3) {
             String b = "Papel";
             String c = "Tijera";
             System.out.println("J1 saco: " + b + "\nJ2 saco: " + c);
             System.out.println("Punto para J2");
             this.p2 += 1;
+            System.out.println();
         } else if (x == 3 && y == 2) {
             String b = "Papel";
             String c = "Tijera";
             System.out.println("J1 saco: " + c + "\nJ2 saco: " + b);
             System.out.println("Punto para J1");
             this.p1 += 1;
+            System.out.println();
         } else if ((x == 1 && y == 1) || (x == 2 && y == 2) || (x == 3 && y == 3) ) {
             String a = "Piedra";
             String b = "Papel";
@@ -73,12 +79,15 @@ public class Opcion {
             if (x == 1 && y == 1){
                 System.out.println("Sacaron " + a);
                 this.empate+=1;
+                System.out.println();
             } else if (x == 2 && y == 2) {
                 System.out.println("Sacaron " + b);
                 this.empate+=1;
+                System.out.println();
             } else if (x == 3 && y == 3){
                 System.out.println("Sacaron " + c);
                 this.empate+=1;
+                System.out.println();
             }
         }
 
@@ -87,25 +96,33 @@ public class Opcion {
             this.p1 = 0;
             this.p2 = 0;
             this.empate = 0;
+            System.out.println();
         } else if ((this.p1 == 1 || this.empate == 1) && this.p2 == 2) {
             System.out.println("El ganador es el jugador 2 con: " + this.p2 + " puntos de 3");
             this.p1 = 0;
             this.p2 = 0;
             this.empate = 0;
+            System.out.println();
         } else if (this.empate == 2 && this.p1 == 1){
             System.out.println("El ganador es el jugador 1 con: " + this.p1 + " puntos de 3");
+            System.out.println();
         } else if (this.empate == 2 && this.p2 == 1) {
             System.out.println("El ganador es el jugador 2 con: " + this.p2 + " puntos de 3");
+            System.out.println();
         } else if (this.empate > 2 ) {
             if (this.p1 == 1){
                 System.out.println("El ganador es el jugador 1 con: " + this.p1 + " punto");
+                System.out.println();
             } else if (this.p2 == 1) {
                 System.out.println("El ganador es el jugador 2 con: " + this.p2 + " punto");
+                System.out.println();
             }else {
                 System.out.println("No hay un ganador aún");
+                System.out.println();
             }
         }else {
             System.out.println("No hay un ganador aún");
+            System.out.println();
         }
 
     }
